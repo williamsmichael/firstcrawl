67610e7edd75b1536c8802a0619f338b4838905f
williamsmichael <willia32@hotmail.com>
2015-12-23 23:51:13 -0800

    Output json file.

--- firstcrawl/spiders/dmoz_spider.py
+++ firstcrawl/spiders/dmoz_spider.py
@@ -6,11 +6,15 @@ class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
     ]
 
     def parse(self, response):
+    	for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+    		url = response.urljoin(href.extract())
+    		yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
     	for sel in response.xpath('//ul/li'):
         	# title = sel.xpath('a/text()').extract()
         	# link = sel.xpath('a/@href').extract()
